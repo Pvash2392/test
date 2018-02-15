87023c1b46e8e282b0071dc40cd1a27b8f7fd715
Pvash <pranshuv@fragmadata.com>
2018-02-15 14:15:04 +0530

    firsrt commit

---
+++ src/main/java/com/fragma/controller/UIController.java
@@ -0,0 +1,13 @@
+package com.fragma.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UIController {
+    @RequestMapping("/")
+    public String home(){
+        return "welcome";
+    }
+}
